fbfd84e8bf45ed19c9a5f0c393108f093eabc42e
Diwakar S <137775205+diwakar3012@users.noreply.github.com>
2025-08-11 14:25:26 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+items = []
+
+@app.route('/items', methods=['GET'])
+def get_items():
+    return jsonify(items)
+
+@app.route('/items', methods=['POST'])
+def add_item():
+    data = request.get_json()
+    items.append(data)
+    return jsonify({"message": "Item added"}), 201
+
+if __name__ == '__main__':
+    app.run(debug=True)
